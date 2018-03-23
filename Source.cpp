d2f90839e8034d968a31cea31566825e52dfbe65
DmitryBorozdin <37050033+DmitryBorozdin@users.noreply.github.com>
2018-03-23 23:37:35 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	string str;   //заводим переменную строки
-	cout << "Ââåäèòå ñòðîêó: ";
+	cout << "Введите строку: ";
 	getline(cin, str);    //ввод строку
 	for (int i = 0; i < str.length(); i++) {
 		if (str[i] == 'a'&&str[i + 1] == 'b'&&str[i + 2] == 'c') {      //условия стоящих под ряд нужных символов
-			cout << "Èñêîìàÿ ñòðîêà: " << str[i] << str[i + 1] << str[i + 2] << endl;        //вывод искомой строки на экран
+			cout << "Искомая строка: " << str[i] << str[i + 1] << str[i + 2] << endl;        //вывод искомой строки на экран
 		}
 	}
 	system("pause");
